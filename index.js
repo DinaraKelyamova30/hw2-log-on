ac3adf57fc58a6f83883e4ac6bdc6ca81c38440e
DinaraKelyamova30 <kelyamova30@gmail.com>
2019-11-03 21:47:41 +0200

    code js

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ if (name == 'Імя') {
     let password = prompt('Введи пароль');
     if (password == 'ЛОГОС') {
         alert('Ласкаво просимо!');
-    } else if (password === null ) {
+    } else if (password == '') {
+        alert('Ви нічого не ввели');
+    } else if (password == null) {
         alert('Вхід скасований');
     } else {
         alert('Пароль невірний');
     }
-} else if (name === null ) {
+} else if (name == null) {
     alert('Вхід скасований');
+} else if (name == '') {
+    alert('Ви нічого не ввели');
 } else {
     alert('Я вас не знаю');
 }
